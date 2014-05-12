1bc23ae863ca7a2f2d535a50f7fe37dd99fa63cc
Jared Smith <jared@iescentral.com>
2014-05-12 16:12:24 -0700

    add routes for Users, Movies, and Favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Popcorn::Application.routes.draw do
+  devise_for :users
   root 'popcorn#index'
   get "popcorn/index"
+  resources :movies
+  resources :favorites
   match '*path' => 'popcorn#index', :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
