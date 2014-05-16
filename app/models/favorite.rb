91ea9ed0eee19d6192c9bd1c4cd61f0c4d549659
Jared Smith <jared@iescentral.com>
2014-05-16 10:58:27 -0700

    Fix typo causing any insert on the favorites table to fail.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ActiveRecord::Base
   belongs_to :movie
   belongs_to :user
 
-  validate_uniqueness_of :user_id, :scope => :movie_id
+  validates_uniqueness_of :user_id, :scope => :movie_id
 end
