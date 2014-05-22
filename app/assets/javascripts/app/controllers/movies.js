a143e0da12d118c7e18e311900aab992c3cc19c0
Jared Smith <jared@iescentral.com>
2014-05-22 15:39:20 -0700

    Adds resource checking if each movie is a favorite for a user

--- app/assets/javascripts/app/controllers/movies.js
+++ app/assets/javascripts/app/controllers/movies.js
@@ -1,10 +1,27 @@
 angular.module('popcornApp.controllers')
 .controller('MoviesController', 
-function($scope, MoviesService, Favorite, UserService) {
-  MoviesService.movies().then(function(movies) {
-      $scope.movies = movies;
-  })
+function($scope, MoviesService, Favorite, UserService, $q) {
   
+  $q.all([UserService.currentUser(), MoviesService.movies()])
+   .then(function(values) {
+      var user = values[0];
+      var movies = values[1];
+    if(user) {
+      var promisedFavorites = _.map(movies, function(movie) {
+        return Favorite.isFavorite(user, movie);
+      });
+
+      $q.all(promisedFavorites).then(function(favorites) {
+        for(var i=0; i<movies.length; i++) {
+          movies[i].isFavorite = favorites[i];
+        }
+        $scope.movies = movies;
+      });
+    } else {
+      $scope.movies = movies;
+    }
+
+    });
   $scope.addFavorite = function(movie) {
   	UserService.currentUser().then(function(user) {
   		Favorite.createForUserAndMovie(user, movie).then(function(){
