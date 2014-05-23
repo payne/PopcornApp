54f39584caddaeaf6657f0c4af7f24f107bf1e32
Jared Smith <jared@iescentral.com>
2014-05-22 17:04:05 -0700

    Add favorite directive and move logic from controller to directive

--- app/assets/javascripts/app/controllers/movies.js
+++ app/assets/javascripts/app/controllers/movies.js
@@ -1,24 +1,8 @@
 angular.module('popcornApp.controllers')
 .controller('MoviesController', 
-function($scope, MoviesService, Favorite, UserService) {
-  MoviesService.movies().then(function(movies) {
-      $scope.movies = movies;
-  })
-  
-  $scope.addFavorite = function(movie) {
-  	UserService.currentUser().then(function(user) {
-  		Favorite.createForUserAndMovie(user, movie).then(function(){
-  			movie.isFavorite = true;
-  		});
-  	});
-    
-  };
+function($scope, MoviesService, Favorite, UserService, $q) {
+  MoviesService.movies().then(function(movies){
+    $scope.movies = movies;
+  });
 
-  $scope.removeFavorite = function(movie) {
-    UserService.currentUser().then(function(user) {
-  		Favorite.removeFavorite(user, movie).then(function(){
-  			movie.isFavorite = false;
-  		});
-  	});
-  };
 });
